dd5b3e6dade2e416cb44447318efc98475b69db6
Ezzat Chamudi <echamudi@outlook.com>
2020-06-27 10:58:57 +0300

    Add download SH

---
+++ dowload.sh
@@ -0,0 +1,3 @@
+curl -o kanjidic2.xml.gz http://www.edrdg.org/kanjidic/kanjidic2.xml.gz
+curl -o JMdict_e.gz http://ftp.monash.edu/pub/nihongo/JMdict_e.gz
+curl -o JMnedict.xml.gz http://ftp.monash.edu/pub/nihongo/JMnedict.xml.gz
